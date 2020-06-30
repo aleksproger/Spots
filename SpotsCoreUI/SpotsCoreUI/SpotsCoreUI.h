23c04f27c12188071f4ca3f6d5d3fdad5707f063
Aleksei Sapitskiy <alecksey12345@gmail.com>
2020-06-30 23:42:47 +0300

    Setup gitignore

---
+++ SpotsCoreUI/SpotsCoreUI/SpotsCoreUI.h
@@ -0,0 +1,19 @@
+//
+//  SpotsCoreUI.h
+//  SpotsCoreUI
+//
+//  Created by Alex on 26.06.2020.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpotsCoreUI.
+FOUNDATION_EXPORT double SpotsCoreUIVersionNumber;
+
+//! Project version string for SpotsCoreUI.
+FOUNDATION_EXPORT const unsigned char SpotsCoreUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotsCoreUI/PublicHeader.h>
+
+
